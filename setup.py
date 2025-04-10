214b9d639158c376d9d14e10b9942ef6a08c9e6e
e0732951 <105834806+e0732951@users.noreply.github.com>
2025-04-10 23:17:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='package',  
     version='1.0.0',
     description='An example package',
-    url='https://github.com/javerngoh/e213d18250504ba584359829bd5deca1/mypackage',
+    url='https://github.com/javerngoh/e213d18250504ba584359829bd5deca1/e213d18250504ba584359829bd5deca1',
     author='javerngoh',
     author_email='e0732951.u.nus.edu',
     packages=find_packages(),
